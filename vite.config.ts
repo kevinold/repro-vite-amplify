4d6b42291dbbcc9cee08f0ec52b416efd5ed7145
Kevin Old <kevin@kevinold.com>
2022-08-22 16:17:25 -0500

    Update with fix from https://github.com/aws-amplify/amplify-js/issues/9639#issuecomment-1081781840

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // per https://github.com/aws-amplify/amplify-js/issues/9639#issuecomment-1081781840
+	resolve: { alias: { './runtimeConfig': './runtimeConfig.browser' } }
 })
